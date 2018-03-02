615a4f919eeef0e9630d1cf069f3fb9c8cff3546
resoliwan <resoliwan@naver.com>
2018-03-02 13:29:03 +0900

    add char type

---
+++ l03/3_5_chartype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main() 
+{
+  using namespace std;
+  char ch;
+
+  cout << "Enter a character:" << endl;
+  cin >> ch;
+  cout << "Your input is: '" << ch << "'"<< endl;
+
+  return 0;
+}
