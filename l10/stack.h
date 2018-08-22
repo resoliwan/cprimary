3415844f6b7f83306fc414a45538d709a9eca1d9
resoliwan <resoliwan@naver.com>
2018-08-22 11:54:21 +0900

    object

---
+++ l10/stack.h
@@ -0,0 +1,21 @@
+#ifndef STACK_H_
+#define STACK_H_
+
+typedef unsigned long Item;
+
+class Stack 
+{
+private:
+  enum {MAX = 10}; 
+  Item items[MAX];
+  int top;
+public:
+  Stack();
+  ~Stack();
+  void push(const Item & item);
+  bool pop(Item & item);
+  bool isempty() const;
+  bool isfull() const;
+};
+
+#endif
